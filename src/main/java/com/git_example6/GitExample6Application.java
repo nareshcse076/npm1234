25238fc71682be2a3255b803caf7a1992d5ec8b9
nareshdemo <npmcse076@gmail.com>
2024-09-22 16:15:51 +0530

    Defect 1245 bug is detected there .Now i have remove it

---
+++ src/main/java/com/git_example6/GitExample6Application.java
@@ -0,0 +1,13 @@
+package com.git_example6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExample6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExample6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_example6/GitExample6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_example6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExample6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
