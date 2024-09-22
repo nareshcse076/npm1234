1d56f1ad337af384b1cb79cd0db45c584fc221c8
nareshdemo <npmcse076@gmail.com>
2024-09-22 16:29:28 +0530

    Bug Fixed -1245 there should not be null value in the database.

--- src/main/java/com/git_example6/A.java
+++ src/main/java/com/git_example6/A.java
@@ -1,4 +1,8 @@
 package com.git_example6;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+    }
 }
---
+++ src/main/java/com/git_example6/B.java
@@ -0,0 +1,8 @@
+package com.git_example6;
+
+public class B {
+    public void test(){
+        System.out.println(300);
+
+    }
+}
